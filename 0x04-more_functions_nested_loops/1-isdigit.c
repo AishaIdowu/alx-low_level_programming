b5d9ecc19a9f898f041c7d98e11ca0c0d3f3a0a6
AishaIdowu <idowueniola@domain.com>
2022-03-18 01:14:39 +0100

    Is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,24 +1,11 @@
 #include "main.h"
-#include<stdio.h>
-#include<ctype.h>
+
 /**
- *_isdigit - checks for a digit.
- *c: input.
- *
- *Return: 1 if c is a digit
- *0 otherwise.
+ * _isdigit - checks for a digit (0 through 9)
+ * @num: int to be checked
+ * Return: 1 if num is a digit, 0 otherwise
  */
-int main()
+int _isdigit(int num)
 {
-
-    int _isdigit(int c);
-    char c;
-    c = '0';
-    printf("Result when numeric character is passed: %d", isdigit(c));
-
-    c = '+';
-    printf("\nResult when non-numeric character is passed: %d", isdigit(c));
-             
-        return 0;   
-    
+	return (num >= '0' && num <= '9');
 }
